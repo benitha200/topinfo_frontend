4f9e4fb4dfa60ba740faaa626e9f792680d3619e
Benitha Louange <benithalouange@MacBook-Pro.local>
2024-12-12 19:35:29 +0200

    changes

--- src/constants/Constants.jsx
+++ src/constants/Constants.jsx
@@ -1,3 +1,3 @@
-// const API_URL='http://localhost:3050/api';
-const API_URL='http://registration.rw/api/api';
+const API_URL='http://localhost:3050/api';
+// const API_URL='http://registration.rw/api/api';
 export default API_URL;
